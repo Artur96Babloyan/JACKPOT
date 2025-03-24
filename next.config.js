9a2fc7b0be73c5aae284bd6ef3b9eaeadd437d4d
Artur96Babloyan <artur11babloyan@gmail.com>
2025-03-24 14:43:19 +0400

    fix: update WalletConnect verification code

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
             key: 'Content-Type',
             value: 'text/plain',
           },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
         ],
       },
     ];
